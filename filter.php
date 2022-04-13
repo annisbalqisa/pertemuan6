3d2c952f0f7db1f0597527dbe231dcc1e34b0d03
annisbalqisa <202410101045@mail.unej.ac.id>
2022-04-14 01:17:49 +0700

    Update filter.php

--- filter.php
+++ filter.php
@@ -6,7 +6,7 @@
         $query = mysqli_query($database, "SELECT * FROM photocard WHERE harga BETWEEN $filter ");
         while ($row = mysqli_fetch_object($query)):
 ?>
-        <div class="col-sm-3 mt-3">
+        <div class="col-sm-auto mt-3">
           <div class="card" style="width: 15rem">
             <img src="<?= $row->gambar; ?>" class="card-img-top" alt="" />
             <div class="card-body">
